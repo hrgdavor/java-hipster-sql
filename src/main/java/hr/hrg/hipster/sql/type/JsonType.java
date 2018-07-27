78f50b5f18c17ca2e8d8da28b991f03eff4aa3d4
hrgdavor <hrgdavor@gmail.com>
2018-07-27 13:18:47 +0200

    remove parent

--- src/main/java/hr/hrg/hipster/sql/type/JsonType.java
+++ src/main/java/hr/hrg/hipster/sql/type/JsonType.java
@@ -11,18 +11,23 @@ public class JsonType<T> implements ICustomType<T>{
 
 	private ObjectMapper mapper;
 	private Class<T> clazz;
+	private ObjectReader reader;
+	private ObjectWriter writer;
 
 	public JsonType(ObjectMapper mapper, Class<T> clazz) {
 		this.mapper = mapper;
+		reader = mapper.readerFor(clazz);
+		writer = mapper.writerFor(clazz);
 		this.clazz = clazz;
 	}
 	
 	@Override
 	public T get(ResultSet rs, int index) throws SQLException {
-		InputStream in = rs.getBinaryStream(index);
-		if(in == null) return null;
+		InputStream src = rs.getBinaryStream(index);
+		if(src == null) return null;
 		try {
-			return mapper.readValue(in, clazz);
+//			return mapper.readValue(in, clazz);
+			return reader.readValue(src);
 		}catch (Exception e) {
 			throw new SQLException("Problem reading result from index "+index+" to convert to "+clazz.getName(),e);
 		}
@@ -34,7 +39,8 @@ public class JsonType<T> implements ICustomType<T>{
 			ps.setNull(index, Types.VARCHAR);
 		}else {
 			try {
-				ps.setString(index, mapper.writeValueAsString(value));
+//				ps.setString(index, mapper.writeValueAsString(value));
+				ps.setString(index, writer.writeValueAsString(value));
 			}catch (Exception e) {
 				throw new SQLException("Problem writing result to index "+index+" converted from "+clazz.getName(),e);
 			}
