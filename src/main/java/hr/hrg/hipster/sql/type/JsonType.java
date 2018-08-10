f2f26e973ecedb6ef738cb18fcb511b9609d80f4
hrgdavor <hrgdavor@gmail.com>
2018-08-10 11:45:58 +0200

    add TypeReference suport to JsonType

--- src/main/java/hr/hrg/hipster/sql/type/JsonType.java
+++ src/main/java/hr/hrg/hipster/sql/type/JsonType.java
@@ -3,22 +3,27 @@ package hr.hrg.hipster.sql.type;
 import java.io.*;
 import java.sql.*;
 
+import com.fasterxml.jackson.core.type.*;
 import com.fasterxml.jackson.databind.*;
 
 import hr.hrg.hipster.sql.*;
 
 public class JsonType<T> implements ICustomType<T>{
 
-	private ObjectMapper mapper;
-	private Class<T> clazz;
+	private String clazz;
 	private ObjectReader reader;
 	private ObjectWriter writer;
 
+	public JsonType(ObjectMapper mapper, TypeReference<T> typeRef) {
+		reader = mapper.readerFor(typeRef);
+		writer = mapper.writerFor(typeRef);
+		this.clazz = typeRef.getType().getTypeName();
+	}
+
 	public JsonType(ObjectMapper mapper, Class<T> clazz) {
-		this.mapper = mapper;
 		reader = mapper.readerFor(clazz);
 		writer = mapper.writerFor(clazz);
-		this.clazz = clazz;
+		this.clazz = clazz.getName();
 	}
 	
 	@Override
@@ -26,10 +31,9 @@ public class JsonType<T> implements ICustomType<T>{
 		InputStream src = rs.getBinaryStream(index);
 		if(src == null) return null;
 		try {
-//			return mapper.readValue(in, clazz);
 			return reader.readValue(src);
 		}catch (Exception e) {
-			throw new SQLException("Problem reading result from index "+index+" to convert to "+clazz.getName(),e);
+			throw new SQLException("Problem reading result from index "+index+" to convert to "+clazz,e);
 		}
 	}
 
@@ -39,10 +43,9 @@ public class JsonType<T> implements ICustomType<T>{
 			ps.setNull(index, Types.VARCHAR);
 		}else {
 			try {
-//				ps.setString(index, mapper.writeValueAsString(value));
 				ps.setString(index, writer.writeValueAsString(value));
 			}catch (Exception e) {
-				throw new SQLException("Problem writing result to index "+index+" converted from "+clazz.getName(),e);
+				throw new SQLException("Problem writing result to index "+index+" converted from "+clazz,e);
 			}
 		}
 	}
